74c393e6f2ee966730b4da623b86c365f4709128
tobae <assumptaodibeli@gmail.com>
2023-11-25 20:27:44 +0100

    first commit

--- clock/app.js
+++ clock/app.js
@@ -1,14 +1,17 @@
 setInterval(() =>{
-    const showDay = document.getElementById('day')
+    const week = document.getElementById('week')
     const month = document.getElementById('month')
-    const todayNum = document.getElementById('date')
+    const today = document.getElementById('day')
 
     date = new Date();
     
-    let dayNum = date.getDate()
-    let day = date.getDay();
-    let presentMonth = date.getMonth();
     let hour = date.getHours();
+
+    if(hour >= 12){
+        document.getElementById('period').innerHTML = 'PM';
+    }else{
+        document.getElementById('period').innerHTML = 'AM';
+    }
     
     let  minute = date.getMinutes();
     if(minute <= 9){
@@ -20,25 +23,22 @@ setInterval(() =>{
         second = `0${second}`
     }
 
-    const hours = document.getElementById('hour').innerHTML = `${hour} :`
-    const minutes = document.getElementById('minutes').innerHTML = minute
-    const seconds = document.getElementById('seconds').innerHTML = second
-
-    // Am OR Pm
-    if(hours < 12){
-        document.getElementById('period').innerHTML = 'am';
-    } else{
-        document.getElementById('period').innerHTML = 'pm';
-    }
+    let currentDate = date.getDate();
+    let currentWeekDay = date.getDay();
+    let currentMonth = date.getMonth();
+    
+    document.getElementById('hour').innerHTML = `${hour} :`
+    document.getElementById('minutes').innerHTML = minute
+    document.getElementById('seconds').innerHTML = second
 
 
     let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-    let weekDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    let weekDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
-    showDay.innerHTML = weekDays[day]
+    week.innerHTML = weekDays[currentWeekDay]
 
-    month.innerHTML = months[presentMonth]
+    month.innerHTML = months[currentMonth]
 
-    todayNum.innerHTML = dayNum
+    today.innerHTML = currentDate
 
 }, 1000); 
